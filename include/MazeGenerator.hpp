539f0b7ad8b432c0427905ef4fc3c05cb38d67a5
salisidris229 <salisuidris229@gmail.com>
2023-06-07 13:21:32 -0700

    header files

---
+++ include/MazeGenerator.hpp
@@ -0,0 +1,40 @@
+#ifndef MAZEGENERATOR_HPP
+#define MAZEGENERATOR_HPP
+
+#include <vector>
+#include <random>
+
+//Base class for maze generators
+//Defines some common fields, virtual methods and implements some common methods
+class MazeGenerator
+{
+public:
+	enum Direction { TOP = 0, BOTTOM = 1, LEFT = 2, RIGHT = 3 };
+
+	MazeGenerator(unsigned int size, std::string seed = "");
+	virtual ~MazeGenerator();
+
+	virtual void generateMaze() = 0;
+	bool** getMazeArray();
+
+	unsigned int getMazeSize();
+	unsigned int getStartX();
+	unsigned int getStartY();
+	unsigned int getEndX();
+	unsigned int getEndY();
+	Direction getEndBorder();
+
+protected:
+	unsigned int mazeSize, startX, startY, endX, endY;
+	bool** mazeArray;
+	bool arrayAllocated;
+	std::mt19937 randomEngine;
+	Direction endBorder;
+
+	virtual void allocateArray() = 0;
+	int getRandomNumber(int min, int max);
+	void setStartPosition();
+	void setExit();
+};
+
+#endif
---
+++ include/ShaderManager.hpp
@@ -0,0 +1,18 @@
+#ifndef SHADERMANAGER_HPP
+#define SHADERMANAGER_HPP
+
+#include "glm.hpp"
+
+class ShaderManager
+{
+private:
+	unsigned int programID;
+
+public:
+	bool loadShaders(const char *vertexShaderPath, const char *fragmentShaderPath);
+	void useShader();
+	void setUniformMatrix4fv(const char *name, glm::mat4 uniform);
+	void setUniformVec3fv(const char *name, glm::vec3 uniform);
+};
+
+#endif
